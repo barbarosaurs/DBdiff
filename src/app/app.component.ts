9617eb33b6e352b2d7fd0f0e55b69383ca0f6212
Filip Barbaric <filip.barbaric@omega-software.eu>
2025-02-24 13:51:08 +0100

    Added nice ui.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,65 +12,69 @@ import { create, BaseDirectory } from "@tauri-apps/plugin-fs";
 import { FileServiseService } from "./file-servise.service";
 import { AppData } from "./app.data";
 //import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
-import * as monaco from "monaco-editor";
 import { diffLines } from "diff";
+import { ButtonModule } from "primeng/button";
+import { AceModule } from "ngx-ace-wrapper";
+import { FormsModule } from "@angular/forms";
+import * as ace from "ace-builds";
+import { ScrollPanelModule } from "primeng/scrollpanel";
+import { Tooltip } from "primeng/tooltip";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    FormsModule,
+    AceModule,
+    ScrollPanelModule,
+    Tooltip,
+  ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent implements AfterViewInit {
-  @ViewChild("editorContainer", { static: false })
+  @ViewChild("curretVersionEdditor")
   editorContainer!: ElementRef<HTMLDivElement>;
 
-  @ViewChild("editorContainer2", { static: false })
-  editorContainer2!: ElementRef<HTMLDivElement>;
-
-  @ViewChild("diffCodeEditor", { static: false })
-  diffCodeEditorHTML!: ElementRef<HTMLDivElement>;
-
-  private currentEdditor?: monaco.editor.IStandaloneCodeEditor;
-  private newEditor?: monaco.editor.IStandaloneCodeEditor;
-  private diffCodeEditor?: monaco.editor.IStandaloneCodeEditor;
   ngAfterViewInit(): void {
+    const aceEditor = ace.edit(this.editorContainer.nativeElement);
+    aceEditor.setFontSize(15);
+    aceEditor.session.setValue("<h1>Ace Editor works great in Angular!</h1>");
     // Create the Monaco Editor instance after the view (DOM) is initialized
-    this.currentEdditor = monaco.editor.create(
-      this.editorContainer.nativeElement,
-      {
-        value: "",
-        language: "javascript",
-        theme: "vs-dark",
-        automaticLayout: true,
-        readOnly: true,
-        minimap: { enabled: false },
-      }
-    );
-
-    this.newEditor = monaco.editor.create(this.editorContainer2.nativeElement, {
-      value: "",
-      language: "javascript",
-      theme: "vs-dark",
-      automaticLayout: true,
-      minimap: { enabled: false },
-    });
-
-    this.diffCodeEditor = monaco.editor.create(
-      this.diffCodeEditorHTML.nativeElement,
-      {
-        value: "",
-        language: "javascript",
-        theme: "vs-dark",
-        automaticLayout: true,
-        readOnly: true,
-        minimap: { enabled: false },
-      }
-    );
-    this.newEditor.getModel()?.onDidChangeContent((newCode) => {
-      this.updateDifference(this.newEditor?.getValue());
-    });
+    // this.currentEdditor = monaco.editor.create(
+    //   this.editorContainer.nativeElement,
+    //   {
+    //     value: "",
+    //     language: "javascript",
+    //     theme: "vs-dark",
+    //     automaticLayout: true,
+    //     readOnly: true,
+    //     minimap: { enabled: false },
+    //   }
+    // );
+    // this.newEditor = monaco.editor.create(this.editorContainer2.nativeElement, {
+    //   value: "",
+    //   language: "javascript",
+    //   theme: "vs-dark",
+    //   automaticLayout: true,
+    //   minimap: { enabled: false },
+    // });
+    // this.diffCodeEditor = monaco.editor.create(
+    //   this.diffCodeEditorHTML.nativeElement,
+    //   {
+    //     value: "",
+    //     language: "javascript",
+    //     theme: "vs-dark",
+    //     automaticLayout: true,
+    //     readOnly: true,
+    //     minimap: { enabled: false },
+    //   }
+    // );
+    // this.newEditor.getModel()?.onDidChangeContent((newCode) => {
+    //   this.updateDifference(this.newEditor?.getValue());
+    // });
   }
 
   fileService = inject(FileServiseService);
@@ -79,21 +83,21 @@ export class AppComponent implements AfterViewInit {
   greetingMessage = "";
 
   updateDifference(newCode: string | undefined) {
-    if (!newCode) return;
-    const differences = diffLines(
-      this.currentEdditor?.getValue()!,
-      this.newEditor?.getValue()!,
-      { ignoreWhitespace: true }
-    );
-    const diffResults = differences
-      .map((part) => {
-        if (part.added) return `+ ${part.value.trim()}`;
-        if (part.removed) return `- ${part.value.trim()}`;
-        return "";
-      })
-      .join("\n");
-    console.log(diffResults);
-    this.diffCodeEditor?.setValue(diffResults);
+    // if (!newCode) return;
+    // const differences = diffLines(
+    //   this.currentEdditor?.getValue()!,
+    //   this.newEditor?.getValue()!,
+    //   { ignoreWhitespace: true }
+    // );
+    // const diffResults = differences
+    //   .map((part) => {
+    //     if (part.added) return `+ ${part.value.trim()}`;
+    //     if (part.removed) return `- ${part.value.trim()}`;
+    //     return "";
+    //   })
+    //   .join("\n");
+    // console.log(diffResults);
+    // this.diffCodeEditor?.setValue(diffResults);
   }
 
   constructor() {
@@ -104,19 +108,18 @@ export class AppComponent implements AfterViewInit {
   async loadAllFiles() {
     await this.fileService.loadDBSchema();
     await this.fileService.loadAllDifs();
-    this.currentEdditor?.setValue(this.appData.dbSchema.content);
-    console.log(this.appData.dbSchema.content);
+    // this.currentEdditor?.setValue(this.appData.dbSchema.content);
+    // console.log(this.appData.dbSchema.content);
   }
 
   async addDiff() {
-    const text = this.diffCodeEditor?.getValue();
-    const newText = this.newEditor?.getValue();
-    if (!text) return;
-    if (!newText) return;
-
-    this.fileService.updateDBSchema(newText);
-    this.fileService.addDiff(text);
-    this.currentEdditor?.setValue(newText);
-    this.diffCodeEditor?.setValue("");
+    // const text = this.diffCodeEditor?.getValue();
+    // const newText = this.newEditor?.getValue();
+    // if (!text) return;
+    // if (!newText) return;
+    // this.fileService.updateDBSchema(newText);
+    // this.fileService.addDiff(text);
+    // this.currentEdditor?.setValue(newText);
+    // this.diffCodeEditor?.setValue("");
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,21 @@ import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { providePrimeNG } from "primeng/config";
+import Aura from "@primeng/themes/aura";
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false || "none",
+        },
+      },
+    }),
+  ],
 };
